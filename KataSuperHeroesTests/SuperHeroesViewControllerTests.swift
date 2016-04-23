d6c55268ac5c6ad4d62d920a4f3f4227471860e6
Carlos Roig <carroisal3@gmail.com>
2016-04-23 11:12:10 +0200

    New test

--- KataSuperHeroesTests/SuperHeroesViewControllerTests.swift
+++ KataSuperHeroesTests/SuperHeroesViewControllerTests.swift
@@ -13,39 +13,58 @@ import UIKit
 @testable import KataSuperHeroes
 
 class SuperHeroesViewControllerTests: AcceptanceTestCase {
-
+    
     private let repository = MockSuperHeroesRepository()
-
+    
     func testShowsEmptyCaseIfThereAreNoSuperHeroes() {
         givenThereAreNoSuperHeroes()
-
+        
         openSuperHeroesViewController()
-
+        
         tester().waitForViewWithAccessibilityLabel("¯\\_(ツ)_/¯")
     }
-
+    
+    func testDoesNotShowEmptyCaseIfThereAreSuperHeroes() {
+        givenThereAreSomeSuperHeroes();
+        
+        openSuperHeroesViewController();
+        
+        tester().waitForAbsenceOfViewWithAccessibilityLabel("¯\\_(ツ)_/¯")
+    }
+    
+    func testShowTheNumberOfSuperHeroes() {
+        givenThereAreSomeSuperHeroes(10)
+        
+        openSuperHeroesViewController()
+        
+        let tableView = tester().waitForViewWithAccessibilityLabel("SuperHeroesTableView") as!  UITableView
+        
+        expect(tableView.numberOfRowsInSection(0)).to(equal(10))
+    }
+    
+    
     private func givenThereAreNoSuperHeroes() {
         givenThereAreSomeSuperHeroes(0)
     }
-
+    
     private func givenThereAreSomeSuperHeroes(numberOfSuperHeroes: Int = 10,
-        avengers: Bool = false) -> [SuperHero] {
+                                              avengers: Bool = false) -> [SuperHero] {
         var superHeroes = [SuperHero]()
         for i in 0..<numberOfSuperHeroes {
             let superHero = SuperHero(name: "SuperHero - \(i)",
-                photo: NSURL(string: "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg"),
-                isAvenger: avengers, description: "Description - \(i)")
+                                      photo: NSURL(string: "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg"),
+                                      isAvenger: avengers, description: "Description - \(i)")
             superHeroes.append(superHero)
         }
         repository.superHeroes = superHeroes
         return superHeroes
     }
-
+    
     private func openSuperHeroesViewController() {
         let superHeroesViewController = ServiceLocator()
             .provideSuperHeroesViewController() as! SuperHeroesViewController
         superHeroesViewController.presenter = SuperHeroesPresenter(ui: superHeroesViewController,
-                getSuperHeroes: GetSuperHeroes(repository: repository))
+                                                                   getSuperHeroes: GetSuperHeroes(repository: repository))
         let rootViewController = UINavigationController()
         rootViewController.viewControllers = [superHeroesViewController]
         presentViewController(rootViewController)
